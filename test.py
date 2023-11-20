8e6db4f9eb9b45e8e1f2c4f0a8f1bd27886756ea
Adam Kubanski <akubansa@gmail.com>
2023-11-20 08:41:38 +0100

    init

---
+++ test.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+import elevenlabs
+import subprocess
+import requests
+import pyperclip
+import os.path
+
+#  pip install urllib3==1.26.6  
+
+load_dotenv()
+api_key = os.getenv("ELEVEN_API_KEY")
+elevenlabs.set_api_key(api_key)
+
+# Get content from clipboard
+clipboard_content = pyperclip.paste()
+#clipboard_content = "Hi, I'm from outer space!"
+
+audio = elevenlabs.generate(
+    text = clipboard_content,
+    voice = "Bella"
+)
+
+
+#Daniel - onwK4e9ZLuTAKqWW03F9
+url = 'https://api.elevenlabs.io/v1/text-to-speech/EXAVITQu4vr4xnSDxMaL'
+headers = {
+    'accept': '*/*',
+    'xi-api-key': api_key,
+    'Content-Type': 'application/json'
+}
+data = {
+    'text': clipboard_content,
+    'voice_settings': {
+        'stability': 0.30,
+        'similarity_boost': 0.50
+    }
+}
+
+
+response = requests.post(url, headers=headers, json=data, stream=True)
+response.raise_for_status()
+# use subprocess to pipe the audio data to ffplay and play it
+ffplay_cmd = ['ffplay', '-autoexit', '-']
+ffplay_proc = subprocess.Popen(ffplay_cmd, stdin=subprocess.PIPE)
+for chunk in response.iter_content(chunk_size=4096):
+    ffplay_proc.stdin.write(chunk)
+    print("Downloading...")
+
+# close the ffplay process when finished
+ffplay_proc.stdin.close()
+ffplay_proc.wait()
